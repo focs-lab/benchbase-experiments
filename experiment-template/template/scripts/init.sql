69dc7d4f05e85b87f8d2a727719020d3ba905872
Daniel Lim <dwslim2@gmail.com>
2025-03-17 06:05:57 +0000

    Restructure

--- experiment-template/template/scripts/init.sql
+++
@@ -1,5 +0,0 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY '1';
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
-CREATE DATABASE benchbase;
-set global max_connections = 2000;
